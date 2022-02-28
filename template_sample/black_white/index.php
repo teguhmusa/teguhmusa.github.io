f85fcb7c4e73446c16751d722eb83e824143f9a9
Teguh Musaharpa Gunawan <teguhmusaharpa@gmail.com>
2022-02-28 17:23:40 +0700

    Update index.php

--- template_sample/black_white/index.php
+++ template_sample/black_white/index.php
@@ -5,7 +5,20 @@
     <meta charset=UTF-8>
     <meta name=viewport content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="../../images/favicon.png">
+    <link rel="shortcut icon" href="../../images/favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="../../icon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../../icon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../../icon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../icon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../../icon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../../icon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../../icon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../../icon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../icon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../../icon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../../icon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../icon/favicon-16x16.png">
     <link rel="canonical" href="https://dindarey.musaharpa.com">
     <meta name=title content="Dinda &amp; Rey Wedding">
     <meta name=author content="musaharpa">
@@ -508,12 +521,12 @@
                     <div class="showComment"></div>
                     <script type="text/javascript">
                         $('.showComment').load("../../php/show_comment.php");
-                        $(document).ready(function(){
-                            $('.btn_1').click(function(){
+                        $(document).ready(function() {
+                            $('.btn_1').click(function() {
                                 var data = $('.form-guest').serialize();
                                 $.ajax({
-                                    type:'POST',
-                                    url:'../../php/master.php?menu=guest&act=simpan',
+                                    type: 'POST',
+                                    url: '../../php/master.php?menu=guest&act=simpan',
                                     data: data,
                                     success: function() {
                                         $('.showComment').load("../../php/show_comment.php");
